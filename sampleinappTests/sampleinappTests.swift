2f9a97d8a417be9d9721ed21bcfebe482c2e48e2
Ryan Hasan <ryan.hasan@gmail.com>
2018-09-27 13:17:05 -0700

    Initial Commit

---
+++ sampleinappTests/sampleinappTests.swift
@@ -0,0 +1,36 @@
+//
+//  sampleinappTests.swift
+//  sampleinappTests
+//
+//  Created by Aminul Hasan on 9/27/18.
+//  Copyright © 2018 Evergent. All rights reserved.
+//
+
+import XCTest
+@testable import sampleinapp
+
+class sampleinappTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
